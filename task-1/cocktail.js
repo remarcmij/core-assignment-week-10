a7f4bc3bc6aca9c8e39e60ce11aa1459a772f5e4
Tester <tester@example.com>
2026-01-28 17:12:46 +0100

    Fail one

--- task-1/cocktail.js
+++ task-1/cocktail.js
@@ -1,14 +1,59 @@
 // API documentation: https://www.thecocktaildb.com/api.php
-
+import fs from 'fs/promises';
 import path from 'path';
 
 const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
 
-// Add helper functions as needed here
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
+}
+
+function createDrinkDetails(drink) {
+  const lines = [];
+
+  lines.push(`## ${drink.strDrink}\n`);
+  if (drink.strDrinkThumb) {
+    lines.push(`![${drink.strDrink}](${drink.strDrinkThumb}/medium)\n`);
+  }
+  lines.push(`**Category**: ${drink.strCategory}\n\n`);
+  // lines.push(
+  //   `**Alcoholic**: ${drink.strAlcoholic == 'Alcoholic' ? 'Yes' : 'No'}\n`
+  // );
+  lines.push(`### Ingredients\n`);
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = drink[`strIngredient${i}`];
+    const measure = drink[`strMeasure${i}`];
+    if (ingredient) {
+      if (measure) {
+        lines.push(`- ${measure.trim()} ${ingredient.trim()}`);
+      } else {
+        lines.push(`- ${ingredient.trim()}`);
+      }
+    }
+  }
+  lines.push(`\n### Instructions\n`);
+  lines.push(`${drink.strInstructions}\n`);
+  lines.push(`Serve in: ${drink.strGlass}\n`);
+  return lines;
+}
+
+function createMarkdownContent(drinks) {
+  let lines = [];
+  lines.push(`# Cocktail Recipes\n`);
+  for (const drink of drinks) {
+    lines = lines.concat(createDrinkDetails(drink));
+  }
+  return lines.join('\n');
+}
 
 export async function main() {
   if (process.argv.length < 3) {
-    console.log('Please provide a cocktail name as a command line argument.');
+    console.error('Please provide a cocktail name as a command line argument.');
     return;
   }
 
@@ -19,15 +64,21 @@ export async function main() {
   const outPath = path.join(__dirname, `../output/${cocktailName}.md`);
 
   try {
-    // 1. Fetch data from the API at the given URL
-    // 2. Generate markdown content to match the examples
-    // 3. Write the generated content to a markdown file as given by outPath
+    const data = await fetchData(url);
+
+    if (data.drinks) {
+      const content = createMarkdownContent(data.drinks);
+      await fs.writeFile(outPath, content, 'utf-8');
+
+      console.log(`Markdown file created for ${cocktailName}`);
+    } else {
+      console.error('No cocktails found with that name.');
+    }
   } catch (error) {
-    // 4. Handle errors
+    console.error(`Error fetching data: ${error.message}`);
   }
 }
 
-// Do not change the code below
 if (!process.env.VITEST) {
   main();
 }
